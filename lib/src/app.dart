98d812e8b60e8737177bf7b9570946092553ff01
100lvlmaster <navinkodag@gmail.com>
2022-05-06 00:12:35 +0530

    remove settings controller

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -8,17 +8,11 @@ import 'package:magicstep/src/pages/sign_in.dart';
 import 'package:magicstep/src/pages/sign_up.dart';
 import 'package:magicstep/src/pages/splash.dart';
 
-import 'settings/settings_controller.dart';
-import 'settings/settings_view.dart';
-
 /// The Widget that configures your application.
 class MyApp extends StatelessWidget {
   const MyApp({
     Key? key,
-    required this.settingsController,
   }) : super(key: key);
-
-  final SettingsController settingsController;
 
   @override
   Widget build(BuildContext context) {
@@ -26,90 +20,81 @@ class MyApp extends StatelessWidget {
     //
     // The AnimatedBuilder Widget listens to the SettingsController for changes.
     // Whenever the user updates their settings, the MaterialApp is rebuilt.
-    return AnimatedBuilder(
-      animation: settingsController,
-      builder: (BuildContext context, Widget? child) {
-        return MaterialApp(
-          // Providing a restorationScopeId allows the Navigator built by the
-          // MaterialApp to restore the navigation stack when a user leaves and
-          // returns to the app after it has been killed while running in the
-          // background.
-          restorationScopeId: 'app',
+    return MaterialApp(
+      // Providing a restorationScopeId allows the Navigator built by the
+      // MaterialApp to restore the navigation stack when a user leaves and
+      // returns to the app after it has been killed while running in the
+      // background.
+      restorationScopeId: 'app',
 
-          // Provide the generated AppLocalizations to the MaterialApp. This
-          // allows descendant Widgets to display the correct translations
-          // depending on the user's locale.
-          // localizationsDelegates: const [
-          //   AppLocalizations.delegate,
-          //   GlobalMaterialLocalizations.delegate,
-          //   GlobalWidgetsLocalizations.delegate,
-          //   GlobalCupertinoLocalizations.delegate,
-          // ],
-          // supportedLocales: const [
-          //   Locale('en', ''), // English, no country code
-          // ],
+      // Provide the generated AppLocalizations to the MaterialApp. This
+      // allows descendant Widgets to display the correct translations
+      // depending on the user's locale.
+      // localizationsDelegates: const [
+      //   AppLocalizations.delegate,
+      //   GlobalMaterialLocalizations.delegate,
+      //   GlobalWidgetsLocalizations.delegate,
+      //   GlobalCupertinoLocalizations.delegate,
+      // ],
+      // supportedLocales: const [
+      //   Locale('en', ''), // English, no country code
+      // ],
 
-          // Use AppLocalizations to configure the correct application title
-          // depending on the user's locale.
-          //
-          // The appTitle is defined in .arb files found in the localization
-          // directory.
-          initialRoute: SplashScreen.routeName,
-          // onGenerateTitle: (BuildContext context) =>
-          //     AppLocalizations.of(context)!.appTitle,
+      // Use AppLocalizations to configure the correct application title
+      // depending on the user's locale.
+      //
+      // The appTitle is defined in .arb files found in the localization
+      // directory.
+      initialRoute: SplashScreen.routeName,
+      // onGenerateTitle: (BuildContext context) =>
+      //     AppLocalizations.of(context)!.appTitle,
 
-          // Define a light and dark color theme. Then, read the user's
-          // preferred ThemeMode (light, dark, or system default) from the
-          // SettingsController to display the correct theme.
-          theme: ThemeData(
-            appBarTheme: AppBarTheme(
-              backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-              elevation: 0,
-              iconTheme: const IconThemeData(
-                color: Colors.black,
-              ),
-              titleTextStyle: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold),
-            ),
+      // Define a light and dark color theme. Then, read the user's
+      // preferred ThemeMode (light, dark, or system default) from the
+      // SettingsController to display the correct theme.
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          elevation: 0,
+          iconTheme: const IconThemeData(
+            color: Colors.black,
           ),
-          // darkTheme: ThemeData.dark(),
-          // themeMode: settingsController.themeMode,
+          titleTextStyle: const TextStyle(
+              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+      // darkTheme: ThemeData.dark(),
+      // themeMode: settingsController.themeMode,
 
-          ///
-          home: const SplashScreen(),
+      ///
+      home: const SplashScreen(),
 
-          // Define a function to handle named routes in order to support
-          // Flutter web url navigation and deep linking.
-          onGenerateRoute: (RouteSettings routeSettings) {
-            return MaterialPageRoute<void>(
-              settings: routeSettings,
-              builder: (BuildContext context) {
-                switch (routeSettings.name) {
-                  case SignInPage.routeName:
-                    return const SignInPage();
-                  case SignUpPage.routeName:
-                    return const SignUpPage();
-                  case HomePage.routeName:
-                    return const HomePage();
-                  case ProductsListPage.routeName:
-                    return const ProductsListPage();
-                  case CreateProduct.routeName:
-                    return CreateProduct(
-                      id: routeSettings.arguments as String?,
-                    );
-                  case PartyListPage.routeName:
-                    return const PartyListPage();
-                  case ReportsPage.routeName:
-                    return const ReportsPage();
-                  case SettingsView.routeName:
-                    return SettingsView(controller: settingsController);
-                  default:
-                    return const SplashScreen();
-                }
-              },
-            );
+      // Define a function to handle named routes in order to support
+      // Flutter web url navigation and deep linking.
+      onGenerateRoute: (RouteSettings routeSettings) {
+        return MaterialPageRoute<void>(
+          settings: routeSettings,
+          builder: (BuildContext context) {
+            switch (routeSettings.name) {
+              case SignInPage.routeName:
+                return const SignInPage();
+              case SignUpPage.routeName:
+                return const SignUpPage();
+              case HomePage.routeName:
+                return const HomePage();
+              case ProductsListPage.routeName:
+                return const ProductsListPage();
+              case CreateProduct.routeName:
+                return CreateProduct(
+                  id: routeSettings.arguments as String?,
+                );
+              case PartyListPage.routeName:
+                return const PartyListPage();
+              case ReportsPage.routeName:
+                return const ReportsPage();
+              default:
+                return const SplashScreen();
+            }
           },
         );
       },
