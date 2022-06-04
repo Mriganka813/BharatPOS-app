352dbeed228bcc0d7c1b829a7f2ac73c4208e1a8
100lvlmaster <navinkodag@gmail.com>
2022-06-05 00:44:43 +0530

    fix: minor

--- lib/src/config/const.dart
+++ lib/src/config/const.dart
@@ -2,8 +2,7 @@ class Const {
   const Const();
 
   ///
-  static const apiUrl =
-      //  'https://blooming-sea-55044.herokuapp.com';
-      'https://0e57-2401-4900-1cc4-5080-2dcb-b2b1-1250-458c.ngrok.io';
+  static const apiUrl = 'https://blooming-sea-55044.herokuapp.com';
+  // 'https://0e57-2401-4900-1cc4-5080-2dcb-b2b1-1250-458c.ngrok.io';
   static const apiV1Url = apiUrl + '/api/v1';
 }
