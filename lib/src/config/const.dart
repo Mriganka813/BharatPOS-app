544fc65f44d78697a49bba673c9c4055cdbadb86
100lvlmaster <navinkodag@gmail.com>
2022-06-10 02:18:33 +0530

    update: version code

--- lib/src/config/const.dart
+++ lib/src/config/const.dart
@@ -2,8 +2,7 @@ class Const {
   const Const();
 
   ///
-  static const apiUrl =
-      // 'https://blooming-sea-55044.herokuapp.com';
-      'https://e5d2-103-167-176-178.ngrok.io';
+  static const apiUrl = 'https://blooming-sea-55044.herokuapp.com';
+  // 'https://2426-103-167-176-178.ngrok.io';
   static const apiV1Url = apiUrl + '/api/v1';
 }
