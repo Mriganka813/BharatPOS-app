1580ee5b3d3a4d9f73ef07d3f814384a97acb661
100lvlmaster <navinkodag@gmail.com>
2022-06-12 14:21:14 +0530

    update version and  version code

--- lib/src/config/const.dart
+++ lib/src/config/const.dart
@@ -3,7 +3,6 @@ class Const {
 
   ///
   static const apiUrl = 'https://api.getshopos.com';
-  // 'https://blooming-sea-55044.herokuapp.com';
   // 'https://0e57-2401-4900-1cc4-5080-2dcb-b2b1-1250-458c.ngrok.io';
   static const apiV1Url = apiUrl + '/api/v1';
 }
