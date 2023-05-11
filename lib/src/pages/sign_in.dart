f2ae8204a412a5426ebdc20233d7dd2b005fe0a7
mriganka boruah <mriganka.81349@gmail.com>
2023-05-11 14:07:58 +0530

    forgot password removed

--- lib/src/pages/sign_in.dart
+++ lib/src/pages/sign_in.dart
@@ -147,24 +147,7 @@ class _SignInPageState extends State<SignInPage> {
                                       ),
                                 ),
                                 const Spacer(),
-                                GestureDetector(
-                                  onTap: () {
-                                    Navigator.pushNamed(
-                                      context,
-                                      '/forgotPassword',
-                                    );
-                                  },
-                                  child: Text(
-                                    "Forgot Password?",
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headline6
-                                        ?.copyWith(
-                                          color: ColorsConst.primaryColor,
-                                          fontWeight: FontWeight.normal,
-                                        ),
-                                  ),
-                                ),
+                                
                               ],
                             ),
                             const SizedBox(height: 5),
