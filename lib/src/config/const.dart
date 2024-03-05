48b09ea451fd77ab16d0f37bf389224a6686ce82
Guru Prakash Kumar <guruprakash745@gmail.com>
2024-03-05 18:45:18 +0530

    const file update

--- lib/src/config/const.dart
+++ lib/src/config/const.dart
@@ -1,14 +1,7 @@
 class Const {
   const Const();
 
-  ///
-  // static const apiUrl = 'http://65.0.7.20:8001';
-  // static const apiUrl = 'https://e484-2405-201-6802-d99c-799e-a09c-8008-42e8.ngrok-free.app';
-  // static const apiUrl = 'https://8702-2409-40e1-1072-a7a3-4854-9f6e-40bf-acc5.ngrok-free.app';
-
-  static const apiUrl = 'https://testbackend-u2af.onrender.com';
-
-  // static const apiUrl = 'http://192.168.215.126:8001';
+    static const apiUrl = 'https://testbackend-u2af.onrender.com';
 
   static const apiV1Url = apiUrl + '/api/v1';
 }
